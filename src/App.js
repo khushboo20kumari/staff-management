cd8bed6391de4cc625fd9a365e05de7711ed7bac
khushboo <khushboo20@navgurukul.org>
2023-10-30 18:22:59 +0530

    add selete options

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-// import NextPage from "./Component/NextPage";
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { Component } from "react";
 import Login from "./Component/Login";
 import RegisterForm from "./Component/RegisterForm";
 import Navbar from "./Navbar";
 import Home from "./Component/Home"
-// import CamparePassword from "./Component/ComparePassword";
 function App() {
-
-  // const getLogin=(loginPassword)=>{
-  //   console.log(loginPassword,"loginPassword")
-
-
-  // }
-
-  // const getSignup=(signupPassword)=>{
-  //       console.log(signupPassword,"signupPassword")
-  // }
-  
-  // console.log(loginPassword,"looo")
 
   return (
 
@@ -30,13 +15,9 @@ function App() {
             <Route path="home" element={<Home />} />
             <Route path="signup"  element={<RegisterForm/>}/>
             <Route path="login" element={<Login/>} />
-         
           </Route>
         </Routes>
       </BrowserRouter>
-     {/* <CamparePassword/> */}
-
-      {/* <NextPage/> */}
     </div>
   )
 }
--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,64 +1,13 @@
 
-import { useState } from "react";
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { useEffect } from "react";
+// import Select from "@mui/material";
+// import MenuItem from "@mui/material";
+import {Select,MenuItem} from '@mui/material';
 import { TextField, Box } from "@mui/material";
 function Login() {
-
-    const [name, setName] = useState('')
-
-    const [signupPassword, setSignupPassword] = useState(null)
-    const [password, setPassword] = useState('')
-
-    const [data, setData] = useState([])
-
-    const fetchDataSignupPassword = () => {
-        fetch("http://localhost:3000/Employee").then((res) => {
-            return res.json()
-        }).then((res) => {
-            setSignupPassword(res)
-        })
-    }
-
-    useEffect(() => {
-        fetchDataSignupPassword()
-    })
-
-    const HandlePassword = (e) => {
-        setPassword(e.target.value)
-
-    }
-
-    const HandleName = (e) => {
-        setName(e.target.value)
-    }
-
-    const HandlerClick = () => {
-        setPassword('')
-        setName('')
-
-        setData([...data, { password }])
-    }
-    const sin=data.map(item=>item.password)
-    for (var i in sin){
-        var a=sin[i]
-         
-    }
-    // console.log(a,"aaaaa")
-
-    const signup = signupPassword.map(item => item.password);
-    
-    console.log(signup)
-    // console.log(signup,"sssss")
-    // var p = signup.includes(a)
-    // console.log(p,"aaaaaa")
-
-
-    
-    
     return (
         <>
 
@@ -78,12 +27,18 @@ function Login() {
                         <Typography variant="h5" style={{ fontSize: "20px" }}>Login Page</Typography>
 
                         <TextField id="outlined-error"
-                            label="Name" onChange={HandleName} value={name}></TextField><br></br>
+                            label="Name" ></TextField><br></br>
 
-                        <TextField id="outlined-error"
-                            label="Password" onChange={HandlePassword} value={password}></TextField><br></br>
+                        <Select value="" displayEmpty style={{width:"65%"}}>
+                            <MenuItem value="">About</MenuItem>
+                            <MenuItem value={1}>BLogs</MenuItem>
+                            <MenuItem value={2}>Contect</MenuItem>
+                        </Select><br></br>
 
-                        <Button variant="contained" onClick={HandlerClick}>Login</Button>
+
+                        <Button style={{marginTop:"20px"}} variant="contained">Login</Button><br></br>
+
+                        <Typography>Do you have an account ?</Typography><Button style={{marginTop:"20px"}} >Sign in </Button><br></br>
 
                     </Box>
 
